4c5ca10103c532889350e243c9dff60619515846
aaaa777 <aaaa777@users.noreply.github.com>
2023-09-04 12:31:21 +0000

    calculate co2 amount

--- FastAPI/src/main.py
+++ FastAPI/src/main.py
@@ -1,14 +1,22 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 
-from .exception.streetview import StreetViewPointNotFound, StreetViewLatitudeOutOfRange, StreetViewLongitudeOutOfRange, StreetViewUnknownError, StreetViewZeroResults
+from .exception.streetview import \
+    StreetViewPointNotFound, \
+    StreetViewLatitudeOutOfRange, \
+    StreetViewLongitudeOutOfRange, \
+    StreetViewUnknownError, \
+    StreetViewZeroResults
 
+from .measuring.co2 import CO2Counter
 
 from .vehicle_counting import VehicleCounting
 from .streetview.streetview import GoogleStreetView
 
 app = FastAPI()
 GSV = GoogleStreetView()
+VC = VehicleCounting(None)
+CC = CO2Counter()
 
 @app.get('/')
 def read_root():
@@ -29,32 +37,43 @@ async def get_streetview_image_path(lat: float, lon: float):
     # error handling
     except StreetViewPointNotFound as e:
         raise HTTPException(status_code=404, detail="StreetViewPoint not found")
+    
     except StreetViewLatitudeOutOfRange as e:
         raise HTTPException(status_code=400, detail="StreetViewLatitude out of range: {}".format(e.lat))
+    
     except StreetViewLongitudeOutOfRange as e:
         raise HTTPException(status_code=400, detail="StreetViewLongitude out of range: {}".format(e.lon))
+    
     except StreetViewUnknownError as e:
         raise HTTPException(status_code=500, detail="StreetViewUnknownError: [{status}] {error_message}".format(status=e.status, error_message=e.error_message))
+    
     except StreetViewZeroResults as e:
         raise HTTPException(status_code=500, detail="StreetViewZeroResults")
+    
     except Exception as e:
         raise HTTPException(status_code=500, detail="Unknown error: {}".format(e))
 
     # Calculation section Here
     try:
-        VC = VehicleCounting(images_dir)
+        VC.set_folder_path(images_dir)
+        result = VC.Count()
+    
     except Exception as e:
         raise HTTPException(status_code=500, detail="Unknown error: {}".format(e))
-    
-    result = VC.Count()
+
     print(result)
     
     # Calculation section Here
-    
+    try:
+        co2_amount = CC.caluculate(result)
+        
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Unknown error: {}".format(e))
     
     return {
         "status": "OK",
-        "CO2": 12345678,
+        "CO2": co2_amount,
         "unit": "ppm",
         "image_path": images_dir,
     }
