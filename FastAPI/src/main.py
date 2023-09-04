aa7e8f3b4261aea2c409f6fabdbf855e164b9523
Not-Applicable-NA <shomcdremi.s@gmail.com>
2023-09-04 15:53:51 +0900

    change image downloader to class method

--- FastAPI/src/main.py
+++ FastAPI/src/main.py
@@ -1,16 +1,20 @@
 from fastapi import FastAPI
 
 from .vehicle_counting import VehicleCounting
-from .streetview.image_downloader import get_images
+from .streetview.streetview import GoogleStreetView
 
 app = FastAPI()
+GSV = GoogleStreetView()
 
 @app.get('/')
 def read_root():
     return {"Hello": "World"}
 
 @app.get('/api/measure_point')
-async def download_image(lon: float, lat: float):
+async def get_streetview_image_path(lat: float, lon: float):
+    
+    stview_image_paths = GSV.get_split_image_paths(lat, lon)
+    GSV.show_image(stview_image_paths)
     
     images_path = get_images(lon, lat)
     VC = VehicleCounting(images_path)
@@ -19,8 +23,4 @@ async def download_image(lon: float, lat: float):
     print(result)
     
     # Calculation section Here
-    
-    return {
-        "CO2": 12345678,
-        "unit": "ppm",
-    }
+    return {'d': stview_image_paths}
