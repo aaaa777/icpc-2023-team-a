2a05be40864edc64228deaede9788ecbbf6ce0e3
aaaa777 <aaaa777@users.noreply.github.com>
2023-09-05 09:48:21 +0000

    impl API test page

--- FastAPI/src/main.py
+++ FastAPI/src/main.py
@@ -15,6 +15,7 @@ from .vehicle_counting import VehicleCounting
 from .streetview.streetview import GoogleStreetView
 
 from pydantic import BaseModel
+from os.path import join, basename, dirname
 
 # /api/measure_area
 class area_req(BaseModel) :
@@ -85,13 +86,19 @@ async def get_streetview_image_path(lat: float, lon: float):
 
     except Exception as e:
         raise HTTPException(status_code=500, detail="Unknown error: {}".format(e))
+    
+    json_boxed_images_paths = ["/" + join(dirname(image_path), "boxed_" + basename(image_path)) for image_path in images_paths]
+    json_images_paths = ["/" + image_path for image_path in images_paths]
 
     return {
         "status": "OK",
         "CO2": co2_amount,
         "unit": "ppm",
         "vehicle_count": result,
-        "image_path": ["/" + image_path for image_path in images_paths],
+        "images_paths": {
+            "original": json_images_paths,
+            "boxed": json_boxed_images_paths,
+        },
     }
 
 
